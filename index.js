f244de30ef67099f8694104d8c749a233b7c6448
Gowtham1802 <gowthama7575@gmail.com>
2024-04-01 12:36:41 +0530

    add

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Count from './Count';
 import Style from './style';
+import Item from './item';
 import {name as appName} from './app.json';
 
 // AppRegistry.registerComponent(appName, () => App);
 // AppRegistry.registerComponent(appName, () => Count);
-AppRegistry.registerComponent(appName, () => Style);
+// AppRegistry.registerComponent(appName, () => Style);
+AppRegistry.registerComponent(appName, () => Item);
